2e0c8bf09258f4475c412cadce7699931ecca420
Nbiba Bedis <bedisnbiba@gmail.com>
2022-03-21 12:04:48 +0100

    Spawn tokio runtime only once

--- task_shell_wrap/src/lib.rs
+++ task_shell_wrap/src/lib.rs
@@ -1,11 +1,16 @@
+use once_cell::sync::Lazy;
+use tokio::runtime::Runtime;
+
+static RUNTIME: Lazy<Runtime> =
+    Lazy::new(|| Runtime::new().expect("Could not spawn tokio runtime"));
+
 #[no_mangle]
 pub fn execute(input: *const u8, len: usize) -> i32 {
     let result = (|| -> Result<i32, Box<dyn std::error::Error>> {
-        let rt = tokio::runtime::Runtime::new()?;
         let input = unsafe { std::slice::from_raw_parts(input, len) };
         let input = std::str::from_utf8(input)?;
 
-        Ok(rt.block_on(deno_task_shell::execute(
+        Ok(RUNTIME.block_on(deno_task_shell::execute(
             deno_task_shell::parser::parse(input)?,
             std::env::vars().collect(),
             &std::env::current_dir()?,
